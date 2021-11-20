192e00cda082eb0b075f4a205871b9fd6c9d34cd
Bluette1 <marylene.sawyer@gmail.com>
2021-11-21 01:32:56 +0200

    Create and add credentials

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '102.220.85.218', user: 'deploy', roles: %w{app db web}
+server '102.220.85.123', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
